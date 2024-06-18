a118579c2a72afe706ea914a28a132e14d4c44a4
Malavika Suresh <malavika.617@gmail.com>
2024-06-18 16:18:03 -0700

    Changes for DB connection and APIs

--- src/main/java/io/getarrays/contactapi/domain/Contact.java
+++ src/main/java/io/getarrays/contactapi/domain/Contact.java
@@ -32,4 +32,5 @@ public class Contact {
     private String address;
     private String status;
     private String photoUrl;
+
 }
--- src/main/java/io/getarrays/contactapi/repo/ContactRepo.java
+++ src/main/java/io/getarrays/contactapi/repo/ContactRepo.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
 @Repository
 public interface ContactRepo extends JpaRepository<Contact, String> {
     Optional<Contact> findById(String id);
